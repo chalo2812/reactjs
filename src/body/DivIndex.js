2656dde739ce009686d93cf8f1110ef16eda5a39
Gonzalo Sola <gonzalosola@gmail.com>
2020-09-26 18:19:07 -0300

    Agregando nuevo DivIndex

---
+++ src/body/DivIndex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DivIndex.css';
+
+class DivIndex extends Component {
+  render() {
+    return (
+      <div className="DivIndex">
+				Index VERGA
+      </div>
+    );
+  }
+}
+
+export default DivIndex;
